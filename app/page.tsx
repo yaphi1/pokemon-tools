72313e8d278f8749c961fbea85ae19c22380795c
Yaphi Berhanu <yaphi1@gmail.com>
2023-09-19 18:20:19 -0400

    Add favicon

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,6 @@ import PokeFinder from "./_components/PokeFinder";
 export default function Home() {
   return (
     <main className="text-xl font-serif text-slate-600">
-      {/* Tailwind test
-      <div className="flex m-4 gap-4">
-        {new Array(3).fill(1).map((_, i) => (
-          <div key={i} className="flex-1 p-4 bg-white shadow-md rounded-md">Card example</div>
-        ))}
-      </div> */}
-
       <PokeFinder />
     </main>
   );
