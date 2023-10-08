21061e0d3e75a2fe75bb475ef64074d68f158be9
Yaphi Berhanu <yaphi1@gmail.com>
2023-10-08 16:28:51 -0400

    Clear local storage after a week

--- app/_utils/dataCleaners.ts
+++ app/_utils/dataCleaners.ts
@@ -35,10 +35,34 @@ export function cleanLocalStorage({ latestUpdate } : { latestUpdate: string }) {
   const lastCleaned = localStorage.getItem('lastCleaned');
   const wasNeverCleaned = !lastCleaned;
   const hasNewUpdate = lastCleaned !== latestUpdate;
-  const shouldClean = wasNeverCleaned || hasNewUpdate;
+  const shouldClean = wasNeverCleaned || hasNewUpdate || isLocalDataStale();
 
   if (shouldClean) {
     localStorage.clear();
     localStorage.setItem('lastCleaned', latestUpdate);
+    localStorage.setItem('lastAutoUpdate', Date.now().toString());
   }
+}
+
+function weeksToMs(weeks: number) {
+  const daysPerWeek = 7;
+  const hoursPerDay = 24;
+  const minutesPerHour = 60;
+  const secondsPerMinute = 60;
+  const msPerSecond = 1000;
+  return weeks *
+    daysPerWeek *
+    hoursPerDay *
+    minutesPerHour *
+    secondsPerMinute *
+    msPerSecond;
+}
+
+function isLocalDataStale() {
+  const lastAutoUpdate = parseInt(localStorage.getItem('lastAutoUpdate') ?? '0');
+  const interval = weeksToMs(1);
+  const currentDate = Date.now();
+  const timeSinceLastUpdate = currentDate - lastAutoUpdate;
+  const isDataStale = timeSinceLastUpdate > interval;
+  return isDataStale;
 }
